ba809d52c29cd4a84ca6e03220ec679c1d916fba
Dishank <mdishank@gmail.com>
2016-08-02 20:52:35 +0530

    Image Grayscaling and Inverting

--- predict.py
+++ predict.py
@@ -31,7 +31,12 @@ pixels = np.array(ima.getdata())
 
 width, height = ima.size
 #pixels = np.array(pixels)
+
+#pixels = pixels[:len(pixels), :1]
+print(pixels.shape)
 pixels = pixels.reshape(1, 784)
+pixels = pixels.astype('float32')
+#pixels /= 255
 
 
 ans = model.predict(x=pixels)
